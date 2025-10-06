e6443956bbc71110d9f188df7cb49173f5227092
Adam Cubel <adam@cubel.org>
2025-10-06 15:43:27 -0500

    [minor] updating db schema

--- database/schema.sql
+++ database/schema.sql
@@ -23,6 +23,15 @@ CREATE INDEX IF NOT EXISTS idx_registration_requests_email ON registration_reque
 CREATE INDEX IF NOT EXISTS idx_registration_requests_status ON registration_requests(status);
 CREATE INDEX IF NOT EXISTS idx_registration_requests_submitted_at ON registration_requests(submitted_at DESC);
 
+-- Function to update updated_at timestamp
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
 -- Trigger to automatically update updated_at
 DROP TRIGGER IF EXISTS update_registration_requests_updated_at ON registration_requests;
 CREATE TRIGGER update_registration_requests_updated_at
@@ -65,15 +74,6 @@ CREATE INDEX IF NOT EXISTS idx_access_requests_user_id ON access_requests(user_i
 CREATE INDEX IF NOT EXISTS idx_access_requests_status ON access_requests(status);
 CREATE INDEX IF NOT EXISTS idx_access_requests_application_id ON access_requests(application_id);
 CREATE INDEX IF NOT EXISTS idx_access_requests_requested_at ON access_requests(requested_at DESC);
-
--- Function to update updated_at timestamp
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = NOW();
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
 
 -- Trigger to automatically update updated_at
 DROP TRIGGER IF EXISTS update_access_requests_updated_at ON access_requests;
